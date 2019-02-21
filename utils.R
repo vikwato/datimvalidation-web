1e9beb37e9b584d182a5a52cfb8b9aff448ff093
Jason P. Pickering <jason.p.pickering@gmail.com>
2019-02-21 14:32:30 +0100

    More

--- utils.R
+++ utils.R
@@ -1,9 +1,10 @@
   require(datimvalidation)
   options(shiny.maxRequestSize=30*1024^2)
+  options("baseurl" = "http://127.0.0.1:8080/")
+  
   DHISLogin<-function(baseurl, username, password) {
     httr::set_config(httr::config(http_version = 0))
-    baseurl<-"http://127.0.0.1:8080/"
-    url <- URLencode(URL = paste0(baseurl, "api/me"))
+    url <- URLencode(URL = paste0(getOption("baseurl"), "api/me"))
     #Logging in here will give us a cookie to reuse
     r <- httr::GET(url ,
                    httr::authenticate(username, password),
@@ -11,6 +12,8 @@
     if(r$status != 200L){
       return(FALSE)
     } else {
+      me <- jsonlite::fromJSON(httr::content(r,as = "text"))
+      options("organisationUnit" = me$organisationUnits$id)
       return(TRUE)
     }
   }
